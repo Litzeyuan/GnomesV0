33602124d364fb21d9324d0f21ee186a00611fd4
lizeyuan <zeyuan.li.home@gmail.com>
2019-04-20 09:34:25 +0100

    added crop and location controllers, and html views

---
+++ src/main/java/gnomes/helper/controllers/CropController.java
@@ -0,0 +1,30 @@
+package gnomes.helper.controllers;
+
+import gnomes.helper.repositories.CropRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CropController {
+
+    private CropRepository cropRepository;
+
+    public CropController(CropRepository cropRepository){
+        this.cropRepository = cropRepository;
+    }
+
+
+    //maps method to  HTTP request path
+    @RequestMapping("/crops")
+    public String getCrops(Model model){
+
+        // Spring will get all crops from db
+        model.addAttribute("crops", cropRepository.findAll());
+
+        return "crops";
+    }
+
+
+
+}
---
+++ src/main/java/gnomes/helper/controllers/LocationCrontroller.java
@@ -0,0 +1,24 @@
+package gnomes.helper.controllers;
+
+import gnomes.helper.repositories.LocationRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LocationCrontroller {
+    private LocationRepository locationRepository;
+
+    public LocationCrontroller(LocationRepository locationRepository){
+        this.locationRepository = locationRepository;
+    }
+
+
+    @RequestMapping("/locations")
+    public String getLocations(Model model){
+
+        model.addAttribute("locations", locationRepository.findAll());
+        return "locations";
+    }
+
+}
